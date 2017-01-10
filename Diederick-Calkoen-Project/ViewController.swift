9b599e17c5b09370883dd815ae015f906eeee308
Diederia <d.w.calkoen@student.vu.nl>
2017-01-10 03:30:12 +0100

    Initial Commit

---
+++ Diederick-Calkoen-Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diederick-Calkoen-Project
+//
+//  Created by Diederick Calkoen on 10/01/17.
+//  Copyright © 2017 Diederick Calkoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
